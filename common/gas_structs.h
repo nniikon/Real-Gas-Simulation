86b81b3379a8edab60b5a49c0dbe373ba3629d74
nniikon <kolya.kasparov@gmail.com>
2024-03-19 00:42:20 +0300

    add define guard to common struct file

--- common/gas_structs.h
+++ common/gas_structs.h
@@ -1,3 +1,6 @@
+#ifndef GAS_STRUCTS_H_
+#define GAS_STRUCTS_H_
+
 #include "stdint.h"
 #include "../glm/glm/glm.hpp"
 
@@ -6,3 +9,5 @@ struct gas_Atoms
     const glm::vec3*   coords;
     const uint64_t   n_coords;
 };
+
+#endif // GAS_STRUCTS_H_ 
