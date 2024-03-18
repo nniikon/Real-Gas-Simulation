63560cbf83b4564e8a0b3290d4b272d08761e151
nniikon <kolya.kasparov@gmail.com>
2024-03-19 01:34:31 +0300

    more appropriate data types

--- common/gas_structs.h
+++ common/gas_structs.h
@@ -6,9 +6,9 @@
 
 struct gas_Atoms
 {
-    const glm::vec3*   coords;
-    const uint64_t   n_coords;
-    const float        radius;
+    const glm::vec3* coords;
+    const size_t     n_coords;
+    const float      radius;
 };
 
 #endif // GAS_STRUCTS_H_ 
