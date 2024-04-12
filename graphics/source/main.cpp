7cd2f72cac486cbe3e62de4dea5b2680119ea12d
fogInSingularity <vladimirnaumov979@gmail.com>
2024-04-12 03:22:54 +0300

    jopa tresnula

--- graphics/source/main.cpp
+++ graphics/source/main.cpp
@@ -20,7 +20,7 @@ int main(const int argc, const char** argv) {
     if (eng_error != ENG_ERR_NO) { fprintf(stderr, "fuck! [ %d ]\n", __LINE__); }
 
     while (!glfwWindowShouldClose(window)) {
-        eng_error = eng_Compute(&atom_list, 0.00001);
+        eng_error = eng_Compute(&atom_list, 0.00001f);
         if (eng_error != ENG_ERR_NO) { fprintf(stderr, "fuck! [ %d ]\n", __LINE__); }        
         Render(&atom, shader_prog_id);
         
